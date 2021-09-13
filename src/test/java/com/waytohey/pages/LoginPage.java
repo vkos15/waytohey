ed627498ac040dfcaefe24443c8fa8cf553f2a49
Valentina <15valentinka@gmail.com>
2021-09-13 09:16:30 +0500

    build gradle

---
+++ src/test/java/com/waytohey/pages/LoginPage.java
@@ -0,0 +1,12 @@
+package com.waytohey.pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class LoginPage {
+
+SelenideElement login=$("#input_login");
+
+
+}
---
+++ src/test/java/com/waytohey/tests/LoginTest.java
@@ -0,0 +1,4 @@
+package com.waytohey.tests;
+
+public class LoginTest {
+}
