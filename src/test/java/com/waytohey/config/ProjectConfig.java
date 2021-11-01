57c32471d6bf654d18959ba99c464c2ea484c79d
Valentina <15valentinka@gmail.com>
2021-11-02 01:53:22 +0500

    add Config.LoadPolicy

--- src/test/java/com/waytohey/config/ProjectConfig.java
+++ src/test/java/com/waytohey/config/ProjectConfig.java
@@ -2,7 +2,7 @@ package com.waytohey.config;
 
 import org.aeonbits.owner.Config;
 
-//@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({"classpath:config/prod.properties"})
 public interface ProjectConfig extends Config {
     String login_correct();
